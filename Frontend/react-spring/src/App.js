a45c98c4111da8d1b7667ae9aba7cbdb19de9535
Nahuel Flores <nahuelhernanflores25@gmail.com>
2022-02-18 20:09:55 -0300

    Modifica el App.js para que funcione el programa

--- Frontend/react-spring/src/App.js
+++ Frontend/react-spring/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Personas from './components/Personas';
 
 const App = () =>
 {
     return(
       <React.Fragment>
-        <h2>Estructura Inicial</h2>
+        <Personas/>
       </React.Fragment>
     );
 }
