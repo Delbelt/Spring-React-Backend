99decaf5ddc25e46b48af0d16c5dffebbdd388df
Nahuel Flores <nahuelhernanflores25@gmail.com>
2022-02-18 19:41:47 -0300

    Crea la estructura inicial del frontend

---
+++ Frontend/react-spring/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () =>
+{
+    return(
+      <React.Fragment>
+        <h2>Estructura Inicial</h2>
+      </React.Fragment>
+    );
+}
+
+export default App
